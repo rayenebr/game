7e2238548899eb1ea4894bd2816b353f8964e291
rayenebr <81468286+rayenebr@users.noreply.github.com>
2021-04-11 20:47:54 +0100

    Delete function.h

--- function.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTION_H_INCLUDED
-#define FUNCTION_H_INCLUDED
-
-void initBack(Background * b);
-void aficherBack(Background b, SDL_Surface * screen);
-void scrolling (background * b, int direction );
-
-#endif // FUNCTION_H_INCLUDED
-
